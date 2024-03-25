77554e23a230ff30edbb2931b4c1f8b318d21b33
jasonpanosso <jasonpanosso@protonmail.com>
2024-03-25 11:53:20 -0400

    refactor Weapon to be used as a child component of Player

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,25 +1,24 @@
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerInput))]
 public class Weapon : MonoBehaviour
 {
     public GameObject projectilePrefab;
     public float fireRate = 0.5f;
-    public float projectileSpawnOffset = 2f;
+    public float projectileSpawnOffset = 0.7f;
+    private float lastFireTime = 0f;
 
     private PlayerInput playerInput;
 
-    private float lastFireTime = 0f;
-
-    public void Fire(Vector2 clickPosition)
+    public void Fire(Vector2 _clickPosition)
     {
         if (Time.time >= lastFireTime + fireRate)
         {
-            Vector2 aimDirection = (clickPosition - (Vector2)transform.position).normalized;
-            Vector2 spawnPoint = (Vector2)transform.position + aimDirection * projectileSpawnOffset;
-
-            GameObject projectile = Instantiate(projectilePrefab, spawnPoint, Quaternion.identity);
-            projectile.GetComponent<Projectile>().Launch(aimDirection);
+            GameObject projectile = Instantiate(
+                projectilePrefab,
+                transform.position,
+                Quaternion.identity
+            );
+            projectile.GetComponent<Projectile>().Launch(transform.up);
 
             lastFireTime = Time.time;
         }
@@ -27,7 +26,31 @@ public class Weapon : MonoBehaviour
 
     private void Awake()
     {
-        playerInput = GetComponent<PlayerInput>();
+        playerInput = GetComponentInParent<PlayerInput>();
+        if (playerInput == null)
+        {
+            Debug.LogError("Weapon component instantiated without PlayerInput component in parent");
+        }
+    }
+
+    private void Update()
+    {
+        UpdateWeaponPosition();
+    }
+
+    // NOTE: we are calculating the weapons position every frame based on the
+    // player's(parent's) transform for future-proofing the case where we want
+    // to have an aiming indicator.
+    private void UpdateWeaponPosition()
+    {
+        Vector2 cursorPosition = playerInput.GetCursorPosition();
+        Vector2 playerPosition = transform.parent.position;
+        Vector2 direction = (cursorPosition - playerPosition).normalized;
+
+        transform.SetPositionAndRotation(
+            playerPosition + direction * projectileSpawnOffset,
+            Quaternion.LookRotation(Vector3.forward, direction)
+        );
     }
 
     private void OnEnable()
