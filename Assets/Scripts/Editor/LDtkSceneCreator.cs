df2a57191e76339572220cebb6b0bdb08f09d217
jasonpanosso <jasonpanosso@protonmail.com>
2024-03-27 19:56:21 -0400

    fix: SceneCreator now updates existing scenes & adds camera prefab to new scenes

--- Assets/Scripts/Editor/LDtkSceneCreator.cs
+++ Assets/Scripts/Editor/LDtkSceneCreator.cs
@@ -6,54 +6,57 @@ using LDtkUnity.Editor;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LDtkSceneCreator : LDtkPostprocessor
 {
     private readonly string levelScenesPath = "Assets/Scenes/Levels/";
     private readonly string allLevelsDataPath = "Assets/Resources/AllLevelsData.asset";
+    private readonly string cameraPrefabPath = "Assets/Prefabs/Cameras.prefab";
 
     private readonly List<LevelData> newLevels = new();
 
     private bool firstRun = true;
+
+    private readonly List<Scene> scenesToSave = new();
 
     protected override void OnPostprocessLevel(GameObject root, LdtkJson projectJson)
     {
         if (firstRun)
         {
             firstRun = false;
-            EditorApplication.delayCall += () =>
-            {
-                if (newLevels.Count == 0)
-                    return;
 
-                var allLevelsData = InitializeAllLevelsData();
-                allLevelsData.Levels.AddRange(newLevels);
-                EditorUtility.SetDirty(allLevelsData);
-                AssetDatabase.SaveAssetIfDirty(allLevelsData);
-            };
+            SaveNewAllLevelsDataAfterDelay();
+            SaveAndCloseScenesAfterDelay();
         }
 
         WorldType worldType = GetWorldTypeFromLevelName(root.name);
         string filePath = Path.Combine(levelScenesPath, worldType.ToString(), root.name + ".unity");
 
-        if (!File.Exists(filePath))
+        if (File.Exists(filePath))
+            EditScene(filePath, root);
+        else
             CreateScene(filePath, root, worldType);
     }
 
-    private WorldType GetWorldTypeFromLevelName(string levelName)
+    private void EditScene(string filePath, GameObject level)
     {
-        foreach (WorldType worldType in Enum.GetValues(typeof(WorldType)))
+        Debug.Log($"Existing level {level.name} found, editing scene {filePath}");
+
+        var scene = EditorSceneManager.OpenScene(filePath, OpenSceneMode.Additive);
+        foreach (var go in scene.GetRootGameObjects())
         {
-            var worldName = worldType.ToString();
-            if (levelName.StartsWith(worldName))
+            if (go.TryGetComponent<LDtkComponentLevel>(out var _))
             {
-                Debug.Log($"Found world type {worldType} for {levelName}");
-                return worldType;
+                GameObject.DestroyImmediate(go);
+                break;
             }
         }
 
-        Debug.LogError("Invalid level name. Level name must be prefixed with a valid WorldType");
-        return WorldType.Grass;
+        level.transform.position = Vector3.zero;
+        GameObject.Instantiate(level, scene).name = "LDtkLevel";
+
+        scenesToSave.Add(scene);
     }
 
     private void CreateScene(string filePath, GameObject level, WorldType worldType)
@@ -67,13 +70,37 @@ public class LDtkSceneCreator : LDtkPostprocessor
         var newScene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Additive);
         newLevels.Add(new(level.name, worldType));
 
+        GameObject cameraPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(cameraPrefabPath);
+        if (cameraPrefab != null)
+        {
+            GameObject instance = PrefabUtility.InstantiatePrefab(cameraPrefab) as GameObject;
+            SceneManager.MoveGameObjectToScene(instance, newScene);
+        }
+        else
+        {
+            Debug.LogError("Failed to load prefab at path: " + cameraPrefabPath);
+        }
+
         level.transform.position = Vector3.zero;
         GameObject.Instantiate(level, newScene).name = "LDtkLevel";
 
-        EditorSceneManager.SaveScene(newScene, filePath);
-
-        EditorSceneManager.CloseScene(newScene, true);
+        scenesToSave.Add(newScene);
         AddSceneToBuildSettings(filePath);
+    }
+
+    private WorldType GetWorldTypeFromLevelName(string levelName)
+    {
+        foreach (WorldType worldType in Enum.GetValues(typeof(WorldType)))
+        {
+            var worldName = worldType.ToString();
+            if (levelName.StartsWith(worldName))
+            {
+                return worldType;
+            }
+        }
+
+        Debug.LogError("Invalid level name. Level name must be prefixed with a valid WorldType");
+        return WorldType.Grass;
     }
 
     private void AddSceneToBuildSettings(string scenePath)
@@ -102,5 +129,34 @@ public class LDtkSceneCreator : LDtkPostprocessor
         }
 
         return allLevelsData;
+    }
+
+    private void SaveNewAllLevelsDataAfterDelay()
+    {
+        EditorApplication.delayCall += () =>
+        {
+            if (newLevels.Count == 0)
+                return;
+
+            var allLevelsData = InitializeAllLevelsData();
+            allLevelsData.Levels.AddRange(newLevels);
+            EditorUtility.SetDirty(allLevelsData);
+            AssetDatabase.SaveAssetIfDirty(allLevelsData);
+        };
+    }
+
+    private void SaveAndCloseScenesAfterDelay()
+    {
+        EditorApplication.delayCall += () =>
+        {
+            if (scenesToSave.Count == 0)
+                return;
+
+            foreach (var scene in scenesToSave)
+            {
+                EditorSceneManager.SaveScene(scene);
+                EditorSceneManager.CloseScene(scene, true);
+            }
+        };
     }
 }
