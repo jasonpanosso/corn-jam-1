ad6e0645189663d0a61ea1f6774959b54ff1a8d8
nwhang1 <nicholasjwhang@gmail.com>
2024-03-28 11:53:08 -0700

    nothing notable

---
+++ Assets/Scripts/PlayScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayScript : MonoBehaviour
+{
+    public void StartLevel0()
+    {
+            LevelManager.Instance.LoadLevel(0);      
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsMenu();
+    
+}
