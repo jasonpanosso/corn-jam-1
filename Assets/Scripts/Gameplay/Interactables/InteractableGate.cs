fc0bc2aac2d6214b959441aede2fd2361ed09a84
jasonpanosso <jasonpanosso@protonmail.com>
2024-04-03 13:16:24 -0400

    feat: buttons & gates working

--- Assets/Scripts/Gameplay/Interactables/InteractableGate.cs
+++ Assets/Scripts/Gameplay/Interactables/InteractableGate.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class InteractableGate : Interactable
 {
     private enum GateState
@@ -8,13 +9,36 @@ public class InteractableGate : Interactable
         Open,
     }
 
+    private Collider2D col;
+
     [SerializeField]
     private GateState state = GateState.Closed;
 
-    public override void Interact(GameObject _)
+    private void Awake()
     {
-        Debug.Log("Gate interacted with");
+        col = GetComponent<Collider2D>();
+        ToggleCollisionBasedOnState();
     }
 
-    private void HandleStateSwitch() { }
+    public override void Interact(GameObject _)
+    {
+        FlipState();
+        ToggleCollisionBasedOnState();
+    }
+
+    private void FlipState()
+    {
+        if (state == GateState.Closed)
+            state = GateState.Open;
+        else
+            state = GateState.Closed;
+    }
+
+    private void ToggleCollisionBasedOnState()
+    {
+        if (state == GateState.Closed)
+            col.enabled = true;
+        else
+            col.enabled = false;
+    }
 }
