bce48d698cfaa6584cbe5feef8ae28cef7870a9a
jasonpanosso <jasonpanosso@protonmail.com>
2024-04-03 15:41:38 -0400

    feat: gate moves : )

--- Assets/Scripts/Gameplay/Interactables/InteractableGate.cs
+++ Assets/Scripts/Gameplay/Interactables/InteractableGate.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
@@ -9,36 +11,103 @@ public class InteractableGate : Interactable
         Open,
     }
 
-    private Collider2D col;
-
     [SerializeField]
     private GateState state = GateState.Closed;
 
+    [SerializeField]
+    private float moveDuration = 1f;
+
+    private Collider2D col;
+    private GameObject lowerGate;
+    private GameObject middleGate;
+    private GameObject upperGate;
+
     private void Awake()
     {
+        InitializeGatePieces();
+
         col = GetComponent<Collider2D>();
-        ToggleCollisionBasedOnState();
-    }
-
-    public override void Interact(GameObject _)
-    {
-        FlipState();
-        ToggleCollisionBasedOnState();
-    }
-
-    private void FlipState()
-    {
-        if (state == GateState.Closed)
-            state = GateState.Open;
-        else
-            state = GateState.Closed;
-    }
-
-    private void ToggleCollisionBasedOnState()
-    {
         if (state == GateState.Closed)
             col.enabled = true;
         else
             col.enabled = false;
+    }
+
+    public override void Interact(GameObject _)
+    {
+        StopAllCoroutines();
+
+        if (state == GateState.Closed)
+        {
+            state = GateState.Open;
+            col.enabled = false;
+            RaiseGate();
+        }
+        else
+        {
+            state = GateState.Closed;
+            col.enabled = true;
+            LowerGate();
+        }
+    }
+
+    private void LowerGate()
+    {
+        var upperY = upperGate.transform.position.y;
+
+        var middleTarget = upperY - 1f;
+        var lowerTarget = upperY - 2f;
+
+        StartCoroutine(MoveCoroutine(middleGate, middleTarget, moveDuration));
+        StartCoroutine(MoveCoroutine(lowerGate, lowerTarget, moveDuration));
+    }
+
+    private void RaiseGate()
+    {
+        var upperY = upperGate.transform.position.y;
+
+        StartCoroutine(MoveCoroutine(middleGate, upperY, moveDuration));
+        StartCoroutine(MoveCoroutine(lowerGate, upperY, moveDuration));
+    }
+
+    private IEnumerator MoveCoroutine(GameObject go, float endY, float duration)
+    {
+        var startPos = go.transform.position;
+        var endPos = upperGate.transform.position;
+        endPos.y = endY;
+
+        var elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            go.transform.position = Vector3.Lerp(startPos, endPos, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        go.transform.position = endPos;
+    }
+
+    private void InitializeGatePieces()
+    {
+        var gatePieces = GetComponentsInChildren<SpriteRenderer>()
+            .Select(sr => sr.gameObject)
+            .ToList();
+
+        if (gatePieces.Count != 3)
+            Debug.LogWarning("Unimplemented: Gates with more than three gate pieces/cubes");
+
+        gatePieces.Sort((a, b) => a.transform.position.y < b.transform.position.y ? 1 : 0);
+
+        upperGate = gatePieces[0];
+        middleGate = gatePieces[1];
+        lowerGate = gatePieces[2];
+
+        if (state == GateState.Open)
+        {
+            middleGate.transform.position = upperGate.transform.position;
+            lowerGate.transform.position = upperGate.transform.position;
+        }
     }
 }
