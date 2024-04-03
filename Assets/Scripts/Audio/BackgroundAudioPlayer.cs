54b66ba4cf8e26f1489c09d3b489edd617320b77
jasonpanosso <jasonpanosso@protonmail.com>
2024-04-03 13:35:34 -0400

    fix: handle menu scene in BackgroundAudioPlayer

--- Assets/Scripts/Audio/BackgroundAudioPlayer.cs
+++ Assets/Scripts/Audio/BackgroundAudioPlayer.cs
@@ -1,8 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackgroundAudioPlayer : MonoBehaviour
 {
     private void OnEnable()
+    {
+        // zz disgusting
+        if (SceneManager.GetActiveScene().name.ToLower() == "menu")
+            PlayMenuBackgroundAudio();
+        else
+            PlayLevelBackgroundAudio();
+    }
+
+    private void PlayMenuBackgroundAudio()
+    {
+        Debug.LogWarning("TODO: Menu background audio");
+    }
+
+    private void PlayLevelBackgroundAudio()
     {
         var worldType = ServiceLocator.LevelManager.CurrentLevel.worldType;
         var musicKey = $"Music_{worldType}";
