a26d01f380ca2450b0806d18db343861eec462e4
jasonpanosso <jasonpanosso@protonmail.com>
2024-04-04 18:28:40 -0400

    feat: laser absorber, setup test in lab 0 : )

---
+++ Assets/Scripts/Gameplay/Lasers/LaserAbsorber.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserAbsorber : MonoBehaviour, ILaserTarget
+{
+    [SerializeField]
+    private List<Interactable> interactables = new();
+
+    public void OnLaserExit()
+    {
+        foreach (var interactable in interactables)
+            if (interactable != null)
+                interactable.Interact(gameObject);
+    }
+
+    public void OnLaserEnter(Direction _)
+    {
+        foreach (var interactable in interactables)
+            if (interactable != null)
+                interactable.Interact(gameObject);
+    }
+}
