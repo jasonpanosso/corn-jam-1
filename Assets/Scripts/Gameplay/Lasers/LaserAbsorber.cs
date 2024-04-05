dbdecceca14b5db833438de42cc8ac4d349b3247
jasonpanosso <jasonpanosso@protonmail.com>
2024-04-05 14:16:13 -0400

    cleanup: simplify LaserAbsorber logic

--- Assets/Scripts/Gameplay/Lasers/LaserAbsorber.cs
+++ Assets/Scripts/Gameplay/Lasers/LaserAbsorber.cs
@@ -6,16 +6,14 @@ public class LaserAbsorber : MonoBehaviour, ILaserTarget
     [SerializeField, InterfaceType(typeof(IInteractable))]
     private Object[] _interactables;
     private IInteractable[] interactables;
+
     private void Awake() => interactables = _interactables.OfType<IInteractable>().ToArray();
 
-    public void OnLaserExit()
-    {
-        foreach (var interactable in interactables)
-            if (interactable != null)
-                interactable.Interact(gameObject);
-    }
+    public void OnLaserExit() => SafelyInteract();
 
-    public void OnLaserEnter(Direction _)
+    public void OnLaserEnter(Direction _) => SafelyInteract();
+
+    private void SafelyInteract()
     {
         foreach (var interactable in interactables)
             if (interactable != null)
