912d2aacee99aed6a8f5f6c068efb8eae6ad8d1d
jasonpanosso <jasonpanosso@protonmail.com>
2024-04-06 12:53:26 -0400

    feat: player death sounds

--- Assets/Scripts/Gameplay/Player/KillPlayer.cs
+++ Assets/Scripts/Gameplay/Player/KillPlayer.cs
@@ -6,13 +6,19 @@ public class KillPlayer : MonoBehaviour
 {
     public UnityEvent OnKill;
 
+    private bool isKilled = false;
+
     public void Kill()
     {
+        if (isKilled)
+            return;
+
+        isKilled = true;
         OnKill.Invoke();
-        StartCoroutine(WaitUntilRestart(3));
+        StartCoroutine(WaitUntilRestart(1.5f));
     }
 
-    private IEnumerator WaitUntilRestart(int seconds)
+    private IEnumerator WaitUntilRestart(float seconds)
     {
         yield return new WaitForSeconds(seconds);
         ServiceLocator.LevelManager.RestartLevel();
